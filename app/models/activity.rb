c5a331f5cd8f8eb95ec459718bc55c725dff4cb6
Yongsun Lee <yslee264@gmail.com>
2019-01-08 17:25:27 +0900

    activity CRUD, remove legal requirement, humanize how_active

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,10 +5,11 @@ class Activity < ApplicationRecord
 
   scope :available, -> {joins(:schedules).where.not(schedules: {id: nil}).uniq}
 
-  validates :member_id, :location, :category, :intro, :overview, :address01, :address02, :zipcode, :legal_requirement, :host_arrangement, :participant_preparation, :notes, :price, :title, :minimum_age, :how_active, :additional_requirement, :group_size, :total_time, presence: true
+  validates :member_id, :location, :category, :intro, :overview, :address01, :address02, :zipcode, :host_arrangement, :participant_preparation, :notes, :price, :title, :minimum_age, :how_active, :additional_requirement, :group_size, :total_time, presence: true
+    # :legal_requirement,
   validate :image_type
-  validates_length_of :intro, minimum: 600
-  validates_length_of :overview, minimum: 1200
+  # validates_length_of :intro, minimum: 600
+  # validates_length_of :overview, minimum: 1200
 
   enum status: [ :draft, :pending, :approved, :signature ]
 
